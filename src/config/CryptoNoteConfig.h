0f51169c1439faba660bae42d904045ff11259e1
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-11-12 20:21:28 +0100

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -68,7 +68,7 @@ namespace CryptoNote
         const uint64_t MINIMUM_FEE                                       =                             UINT64_C(1); // "basicly" feeless
         /* This section defines our minimum and maximum mixin counts required for transactions */
         const uint64_t MINIMUM_MIXIN_V1                        =                                      0;
-        const uint64_t MAXIMUM_MIXIN_V1                        =                                     10;
+        const uint64_t MAXIMUM_MIXIN_V1                        =                                     1000;
 
         const uint64_t MINIMUM_MIXIN_V2                        =                                      0;
         const uint64_t MAXIMUM_MIXIN_V2                        =                                      5;
