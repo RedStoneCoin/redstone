359592d73c4d5344bec368eb64c40ff963d44358
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-11-16 18:55:46 +0100

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -65,16 +65,16 @@ namespace CryptoNote
         const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE            =                                       600;
 
         const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT                  =                                         6;
-        const uint64_t MINIMUM_FEE                                       =                             UINT64_C(1); // "basicly" feeless
+        const uint64_t MINIMUM_FEE                                       =                             UINT64_C(499); // "basicly" feeless
         /* This section defines our minimum and maximum mixin counts required for transactions */
         const uint64_t MINIMUM_MIXIN_V1                        =                                      0;
-        const uint64_t MAXIMUM_MIXIN_V1                        =                                     10;
+        const uint64_t MAXIMUM_MIXIN_V1                        =                                      1;
 
         const uint64_t MINIMUM_MIXIN_V2                        =                                      0;
-        const uint64_t MAXIMUM_MIXIN_V2                        =                                      5;
+        const uint64_t MAXIMUM_MIXIN_V2                        =                                      1;
 
-        const uint64_t MINIMUM_MIXIN_V3                        =                                      2;
-        const uint64_t MAXIMUM_MIXIN_V3                        =                                      2;
+        const uint64_t MINIMUM_MIXIN_V3                        =                                      0;
+        const uint64_t MAXIMUM_MIXIN_V3                        =                                      1;
 
         /* The heights to activate the mixin limits at */
         const uint32_t MIXIN_LIMITS_V1_HEIGHT                  =                                 5;
@@ -83,8 +83,8 @@ namespace CryptoNote
 
         const uint64_t DEFAULT_MIXIN_V0                        =                                      1;
         const uint64_t DEFAULT_MIXIN_V1                        =                                      1;
-        const uint64_t DEFAULT_MIXIN_V2                        =                                      3;
-        const uint64_t DEFAULT_MIXIN_V3                        =                                      2;
+        const uint64_t DEFAULT_MIXIN_V2                        =                                      1;
+        const uint64_t DEFAULT_MIXIN_V3                        =                                      1;
 
         const uint64_t DEFAULT_DUST_THRESHOLD                  =                          UINT64_C(100);
         const uint64_t DEFAULT_DUST_THRESHOLD_V2               =                            UINT64_C(0);
